acb0ff89a7c5e87f931c4bbb0414092a97582427
bachlex1608 <lxbachit03.working@gmail.com>
2025-02-12 22:14:37 +0700

    first commit

---
+++ Jwt.Authorization/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT.Authentication.Authorization.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    [HttpGet("read")]
+    public async Task<IActionResult> GetMe(CancellationToken cancellationToken = default)
+    {
+        await Task.CompletedTask;
+
+        return Ok("Read profile");
+    }
+}
---
+++ Jwt.Authorization/Program.cs
@@ -0,0 +1,44 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = builder?.Configuration["JwtSettings:Issuers"] ?? "default-issuers",
+                        ValidAudience = builder?.Configuration["JwtSettings:Audience"] ?? "default-audience",
+                        ClockSkew = TimeSpan.Zero,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder?.Configuration["JwtSettings:SecretKey"] ?? "default-secret-key"))
+                    };
+                });
+builder.Services.AddAuthentication();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
